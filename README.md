c8727902c5ff11b660f000b21a2d81ffa7759e3e
Qingzhi Ma <qingzma@gmail.com>
2023-05-02 15:40:21 +0800

    add bloom filter

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # card
+
+please first install
+
+1. https://github.com/hyrise/sql-parser
+2. https://github.com/mavam/libbf
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#include <bloom_filter.hpp>
+#include <bloom_filter/basic.hpp>
+#include <bloom_filter/counting.hpp>
+#include <iostream>
 #include <stdlib.h>
 #include <string>
 
@@ -12,6 +16,21 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Usage: ./card \"SELECT * FROM test;\"\n");
     return -1;
   }
+
+  // bf::basic_bloom_filter b(0.8, 100);
+  bf::counting_bloom_filter b(bf::make_hasher(5), 1000000, 16);
+  b.clear();
+  b.add("foo");
+  b.add(32);
+  b.add("foo");
+  b.add(33);
+  b.add("foo");
+
+  std::cout << b.lookup("foo") << std::endl;
+  std::cout << b.lookup(32) << std::endl;
+  std::cout << b.lookup(33) << std::endl;
+  std::cout << b.lookup(40) << std::endl;
+
   std::string query = argv[1];
 
   // parse a given query
@@ -32,7 +51,7 @@ int main(int argc, char *argv[]) {
       if (statement->isType(hsql::kStmtSelect)) {
         const auto *select =
             static_cast<const hsql::SelectStatement *>(statement);
-        printf("haha %s", select->fromTable->getName());
+        // printf("haha %s", select->fromTable->getName());
       }
     }
     return 0;
